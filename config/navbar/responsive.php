c0ba48cb9b1176ed76480a83c9ba29a79fd646f6
Eric Johansson <johanssoneric5@gmail.com>
2018-12-12 22:22:57 +0100

    removing menu items from responsive navbar

--- config/navbar/responsive.php
+++ config/navbar/responsive.php
@@ -19,25 +19,6 @@ return [
             "text" => "Redovisning",
             "url" => "redovisning",
             "title" => "Redovisningstexter från kursmomenten.",
-            "submenu" => [
-                "items" => [
-                    [
-                        "text" => "Kmom01",
-                        "url" => "redovisning/kmom01",
-                        "title" => "Redovisning för kmom01.",
-                    ],
-                    [
-                        "text" => "Kmom02",
-                        "url" => "redovisning/kmom02",
-                        "title" => "Redovisning för kmom02.",
-                    ],
-                    [
-                        "text" => "Kmom03",
-                        "url" => "redovisning/kmom03",
-                        "title" => "Redovisning för kmom03.",
-                    ],
-                ],
-            ],
         ],
         [
             "text" => "Om",
